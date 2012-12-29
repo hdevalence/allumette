0965da1e0172db203733d732f37bfcf87e8ea174
Henry de Valence <hdevalence@hdevalence.ca>
2012-12-29 17:01:55 -0500

    Rename AllumetteStacker -> AllumetteEngine.

---
+++ src/lib/AllumetteEngine.cpp
@@ -0,0 +1,13 @@
+#include "AllumetteEngine.h"
+
+AllumetteEngine::AllumetteEngine(const QJsonArray& params, QObject *parent)
+	: QObject(parent),
+	  m_params(params)
+{
+
+}
+
+void AllumetteEngine::stack()
+{
+
+}
--- src/lib/AllumetteEngine.h
+++ src/lib/AllumetteEngine.h
@@ -1,20 +1,20 @@
-#ifndef ALLUMETTESTACKER_H
-#define ALLUMETTESTACKER_H
+#ifndef ALLUMETTEENGINE_H
+#define ALLUMETTEENGINE_H
 
 #include <QtCore/QObject>
 #include <QtCore/QJsonArray>
 
-class AllumetteStacker : public QObject
+class AllumetteEngine : public QObject
 {
 	Q_OBJECT
 public:
 
 	/**
-	 * @brief AllumetteStacker does the work of image stacking.
+     * @brief AllumetteEngine does the work of image stacking.
 	 * @param params A JSON array containing the stacking parameters.
 	 * @param parent The parent QObject
 	 */
-	explicit AllumetteStacker(const QJsonArray& params, QObject *parent = 0);
+    explicit AllumetteEngine(const QJsonArray& params, QObject *parent = 0);
 
 	void stack();
 	
@@ -26,4 +26,4 @@ private:
 	QJsonArray m_params;
 };
 
-#endif // ALLUMETTESTACKER_H
+#endif // ALLUMETTEENGINE_H
